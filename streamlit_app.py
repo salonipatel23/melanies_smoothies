990d13721a7b3072c6d2bc4a26116daecf43a330
salonipatel23 <saloni.patel@lirik.io>
2025-01-13 14:06:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,10 +42,10 @@ if ingredients_list:
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-        values ('""" + ingredients_string.strip() + """', '""" + name_on_order + """')"""
+        values ('"""+ ingredients_string.strip() +"""', '""" + name_on_order + """')"""
 
 
-    # st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
     time_to_insert = st.button("Submit Order")
     
     if time_to_insert:
